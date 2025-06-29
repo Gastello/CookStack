c3ec0c06d05e188dfe1cf387a4c60167d262224a
Gastello <maximgst@gmail.com>
2025-06-30 01:14:01 +0300

    Add user authentication check in LayoutSidebar; redirect to login if user is not authenticated.

--- src/routes/layouts/LayoutSidebar.tsx
+++ src/routes/layouts/LayoutSidebar.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { useUserStore } from "../../App";
 
 export default function LayoutSidebar() {
+  const { user } = useUserStore();
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
   return (
     <div className="flex h-full">
       <Sidebar />
