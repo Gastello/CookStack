9447abcfc30d5e0b9cf472402d26c89addb501ca
Gastello <maximgst@gmail.com>
2025-07-05 00:03:23 +0300

    dishesStore works

--- src/pages/Dishes.tsx
+++ src/pages/Dishes.tsx
@@ -12,15 +12,11 @@ export default function Dishes() {
   const { fetchDishes } = useDishesStore();
   const { loading } = useDishesStore();
   const { addDish } = useDishesStore();
+  const { updateDish } = useDishesStore();
+  const { removeDish } = useDishesStore();
+  const { getDishById } = useDishesStore();
+
   const { fetchTags } = useTagsStore();
-
-  const tagsLoading = useTagsStore((s) => s.loading);
-  const tags = useTagsStore((s) => s.tags);
-
-  const addTag = useTagsStore((s) => s.addTag);
-  const updateTag = useTagsStore((s) => s.updateTag);
-  const removeTag = useTagsStore((s) => s.removeTag);
-  const getTagById = useTagsStore((s) => s.getTagById);
 
   useEffect(() => {
     fetchTags();
@@ -42,58 +38,7 @@ export default function Dishes() {
       {loading ? (
         <Loader loading={true} size={256} name={LOADER_EMOJIES.forkAndKnife} />
       ) : dishes && dishes.length != 0 ? (
-        <div>
-          <DishesDashboard />
-          <div>
-            {!tagsLoading
-              ? tags.map((x) => (
-                  <div key={x.id}>
-                    {x.color} {x.text}
-                  </div>
-                ))
-              : ""}
-          </div>
-          <div className="*:p-2 flex gap-2 *:bg-amber-400">
-            <button
-              onClick={() => {
-                const chars =
-                  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-                let result = "";
-                const length = 10;
-                for (let i = 0; i < length; i++) {
-                  result += chars.charAt(
-                    Math.floor(Math.random() * chars.length)
-                  );
-                }
-                addTag(result, "#000000");
-              }}
-            >
-              CREATE
-            </button>
-            <button
-              onClick={() => {
-                const id = tags[0].id;
-                const tag = getTagById(id);
-                if (tag) {
-                  updateTag({
-                    color: "#000000",
-                    id: id,
-                    text: tag.text === "Test" ? "Updated" : "Test",
-                  });
-                }
-              }}
-            >
-              UPDATE
-            </button>
-            <button
-              onClick={() => {
-                removeTag(tags[0].id);
-              }}
-            >
-              DELETE
-            </button>
-          </div>
-        </div>
+        <DishesDashboard />
       ) : (
         <EmptyDashboard
           emoji={EMOJI.forkAndKnife}
