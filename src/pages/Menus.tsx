abf37f72aa7ed5faefa1212f396566a0656bfd03
Gastello <maximgst@gmail.com>
2025-07-30 15:04:33 +0300

    Add Menus component with loading state and menus fetching logic

--- src/pages/Menus.tsx
+++ src/pages/Menus.tsx
@@ -1,12 +1,23 @@
+import { useEffect } from "react";
 import Button from "../components/button/Button";
 import DashboardHeader from "../components/dashboardHeader/DashboardHeader";
 import { EMOJI } from "../components/emoji/Emoji";
 import EmptyDashboard from "../components/emptyDashboard/EmptyDashboard";
+import Loader, { LOADER_EMOJIES } from "../components/loader/Loader";
 import MenusDashboard from "../components/menusDashboard/MenusDashboard";
+import { useMenusStore } from "../store/menusStore";
 
 export default function Menus() {
+  const menus = useMenusStore((s) => s.menus);
+  const loading = useMenusStore((s) => s.loading);
+  const fetchMenus = useMenusStore((s) => s.fetchMenus);
+
+  useEffect(() => {
+    fetchMenus();
+  }, [fetchMenus]);
+
   return (
-    <div className="flex flex-col h-full p-[30px]">
+    <div className="relative flex flex-col h-full p-[30px]">
       <DashboardHeader
         title="My Menus"
         subtitle="Create and manage your meal plans"
@@ -19,7 +30,9 @@ export default function Menus() {
         />
       </DashboardHeader>
 
-      {true ? (
+      {loading ? (
+        <Loader loading={true} size={256} name={LOADER_EMOJIES.memo} />
+      ) : menus && menus.length != 0 ? (
         <MenusDashboard />
       ) : (
         <EmptyDashboard
